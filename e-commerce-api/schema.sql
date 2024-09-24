f038a0309744e139bde3db3a737c865d14059187
Aditya Pathak <adityapathak1189@gmail.com>
2024-09-24 06:15:08 +0530

    db schmema

--- e-commerce-api/schema.sql
+++ e-commerce-api/schema.sql
@@ -28,39 +28,45 @@ CREATE TABLE IF NOT EXISTS inventory (
 );
 
 CREATE TABLE IF NOT EXISTS carts (
-    id CHAR(30) PRIMARY KEY
-    user_id CHAR(30) NOT NULL FOREIGN KEY REFERENCES users(id),
+    id CHAR(30) PRIMARY KEY,
+    user_id CHAR(30) NOT NULL,
     cart_state ENUM('CART_CREATED', 'CART_COMPLETED', 'CART_CANCELED') DEFAULT('CART_CREATED'),
     cart_amount FLOAT NOT NULL,
     created_at TIMESTAMP DEFAULT(NOW()),
-    updated_at TIMESTAMP DEFAULT(NOW())
+    updated_at TIMESTAMP DEFAULT(NOW()),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS cart_items (
     id CHAR(30) PRIMARY KEY,
-    cart_id CHAR(30) NOT NULL FOREIGN KEY REFERENCES carts(id),
-    product_id CHAR(30) NOT NULL FOREIGN KEY REFERENCES products(id),
+    cart_id CHAR(30) NOT NULL,
+    product_id CHAR(30) NOT NULL,
     quantity INT NOT NULL,
     created_at TIMESTAMP DEFAULT(NOW()),
-    updated_at TIMESTAMP DEFAULT(NOW())
+    updated_at TIMESTAMP DEFAULT(NOW()),
+    FOREIGN KEY (cart_id) REFERENCES carts(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 CREATE TABLE IF NOT EXISTS orders (
     id CHAR(30) PRIMARY KEY,
-    user_id CHAR(30) NOT NULL FOREIGN KEY REFERENCES users(id), 
-    cart_id CHAR(30) NOT NULL FOREIGN KEY REFERENCES carts(id),
+    user_id CHAR(30) NOT NULL, 
+    cart_id CHAR(30) NOT NULL,
     total_price FLOAT NOT NULL,
     order_state ENUM('ORDER_CREATED', 'ORDER_COMPLETED', 'ORDER_CANCELLED') DEFAULT('ORDER_CREATED'),
     created_at TIMESTAMP DEFAULT(NOW()),
-    updated_at TIMESTAMP DEFAULT(NOW())
+    updated_at TIMESTAMP DEFAULT(NOW()),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (cart_id) REFERENCES carts(id)
 );
 
 CREATE TABLE IF NOT EXISTS payments (
     id CHAR(30) PRIMARY KEY,
-    order_id INT NOT NULL FOREIGN KEY REFERENCES orders(id),
+    order_id CHAR(30) NOT NULL,
     payment_method ENUM('STRIPE', 'RAZORPAY', 'PAYPAL') NOT NULL, -- For now we only have stripe
     payment_state ENUM('PAYMENT_PENDING', 'PAYMENT_SUCCESS', 'PAYMENT_FAILED', 'PAYMENT_CANCELED') DEFAULT('PAYMENT_PENDING'),
     created_at TIMESTAMP DEFAULT(NOW()),
-    updated_at TIMESTAMP DEFAULT(NOW())
+    updated_at TIMESTAMP DEFAULT(NOW()),
+    FOREIGN KEY (order_id) REFERENCES orders(id)
 );
 
