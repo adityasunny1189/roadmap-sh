5698b4535d2332d81bee17944561d6a1efe9f2bc
Aditya Pathak <adityapathak@Adityas-Mac-mini.local>
2024-09-28 12:45:37 +0530

    modified schema

--- e-commerce-api/schema.sql
+++ e-commerce-api/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE my_commerce;
 USE my_commerce;
 
 CREATE TABLE IF NOT EXISTS users (
-    id CHAR(30) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     full_name VARCHAR(200) GENERATED ALWAYS AS (CONCAT(first_name, ' ', last_name)),
@@ -13,28 +13,37 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMP DEFAULT(NOW())
 );
 
+CREATE TABLE IF NOT EXISTS categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(100) NOT NULL,
+    category_description TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS products (
-    id CHAR(30) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     product_description TEXT NOT NULL,
-    category VARCHAR(100) NOT NULL,
     price FLOAT NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    image_url VARCHAR(255) NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS inventory (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     product_id CHAR(30) NOT NULL, 
-    quantity INT NOT NULL
+    quantity INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS carts (
     id CHAR(30) PRIMARY KEY,
-    user_id CHAR(30) NOT NULL,
+    user_id INT NOT NULL,
     cart_state ENUM('CART_CREATED', 'CART_COMPLETED', 'CART_CANCELED') DEFAULT('CART_CREATED'),
     cart_amount FLOAT NOT NULL,
     created_at TIMESTAMP DEFAULT(NOW()),
     updated_at TIMESTAMP DEFAULT(NOW()),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS cart_items (
@@ -44,29 +53,29 @@ CREATE TABLE IF NOT EXISTS cart_items (
     quantity INT NOT NULL,
     created_at TIMESTAMP DEFAULT(NOW()),
     updated_at TIMESTAMP DEFAULT(NOW()),
-    FOREIGN KEY (cart_id) REFERENCES carts(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
+    FOREIGN KEY (cart_id) REFERENCES carts(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-    id CHAR(30) PRIMARY KEY,
-    user_id CHAR(30) NOT NULL, 
+    id INT PRIMARY KEY,
+    user_id INT NOT NULL, 
     cart_id CHAR(30) NOT NULL,
     total_price FLOAT NOT NULL,
     order_state ENUM('ORDER_CREATED', 'ORDER_COMPLETED', 'ORDER_CANCELLED') DEFAULT('ORDER_CREATED'),
     created_at TIMESTAMP DEFAULT(NOW()),
     updated_at TIMESTAMP DEFAULT(NOW()),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (cart_id) REFERENCES carts(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (cart_id) REFERENCES carts(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS payments (
-    id CHAR(30) PRIMARY KEY,
-    order_id CHAR(30) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
     payment_method ENUM('STRIPE', 'RAZORPAY', 'PAYPAL') NOT NULL, -- For now we only have stripe
     payment_state ENUM('PAYMENT_PENDING', 'PAYMENT_SUCCESS', 'PAYMENT_FAILED', 'PAYMENT_CANCELED') DEFAULT('PAYMENT_PENDING'),
     created_at TIMESTAMP DEFAULT(NOW()),
     updated_at TIMESTAMP DEFAULT(NOW()),
-    FOREIGN KEY (order_id) REFERENCES orders(id)
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
 );
 
