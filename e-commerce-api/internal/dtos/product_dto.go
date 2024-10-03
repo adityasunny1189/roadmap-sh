54ea2c692313109030014fa51e9578f662b5fa88
Aditya Pathak <adityapathak1189@gmail.com>
2024-10-03 06:26:08 +0530

    product dto

---
+++ e-commerce-api/internal/dtos/product_dto.go
@@ -0,0 +1,39 @@
+package dtos
+
+import "github.com/adityasunny1189/roadmap-sh/e-commerce-api/internal/core/domain"
+
+type AddNewProductRequest struct {
+	ProductName        string  `json:"product_name"`
+	ProductDescription string  `json:"product_description"`
+	CategoryId         int64   `json:"category_id"`
+	ImageURL           string  `json:"image_url"`
+	Price              float64 `json:"price"`
+}
+
+type AddNewProductResponse struct {
+	Product domain.Product `json:"product"`
+}
+
+type UpdateInventoryRequest struct {
+	ProductId int64 `json:"product_id"`
+	Quantity  int64 `json:"quantity"`
+}
+
+type UpdateInventoryResponse struct {
+	CurrentQuantity int64 `json:"current_quantity"`
+}
+
+type SortAndFilterOption string
+
+const (
+	SORT_BY_PRICE    SortAndFilterOption = "SORT_BY_PRICE"
+	SORT_BY_CATEGORY SortAndFilterOption = "SORT_BY_CATEGORY"
+)
+
+type SortAndFilterProductRequest struct {
+	SortByOption SortAndFilterOption `json:"sort_by_option"`
+}
+
+type SortAndFilterProductResponse struct {
+	Products []domain.Product `json:"products"`
+}
--- e-commerce-api/internal/transport/http/http_handler_helper.go
+++ e-commerce-api/internal/transport/http/http_handler_helper.go
@@ -1,2 +1,76 @@
 package http
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/adityasunny1189/roadmap-sh/e-commerce-api/internal/dtos"
+)
+
+func SendJsonResponse(w http.ResponseWriter,
+	status int,
+	data interface{},
+	errorHandlingDetails interface{}) {
+	apiResponse := dtos.ApiResponse{
+		StatusCode:           status,
+		Data:                 data,
+		ErrorHandlingDetails: errorHandlingDetails,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(apiResponse)
+}
+
+func SendErrorResponse(w http.ResponseWriter, errorType dtos.ErrorType, errorMsg string) {
+	var errorHandlingDetails dtos.ErrorHandlingDetails
+	var errorStatusCode int
+
+	switch errorType {
+	case dtos.POPUP:
+		errorStatusCode = 404
+		errorHandlingDetails = dtos.ErrorHandlingDetails{
+			ErrorType: dtos.POPUP,
+			ErrorDetails: dtos.PopUpErrorDetails{
+				Message: errorMsg,
+			},
+		}
+	case dtos.TOAST:
+		errorStatusCode = 404
+		errorHandlingDetails = dtos.ErrorHandlingDetails{
+			ErrorType: dtos.TOAST,
+			ErrorDetails: dtos.ToastErrorDetails{
+				ToastTimeoutInSec: 5,
+				ToastColor:        "RED",
+				Message:           errorMsg,
+			},
+		}
+	case dtos.IN_SCREEN:
+		errorStatusCode = 404
+		errorHandlingDetails = dtos.ErrorHandlingDetails{
+			ErrorType: dtos.IN_SCREEN,
+			ErrorDetails: dtos.InScreenErrorDetails{
+				Message: errorMsg,
+			},
+		}
+	case dtos.SNACK_BAR:
+		errorStatusCode = 404
+		errorHandlingDetails = dtos.ErrorHandlingDetails{
+			ErrorType: dtos.SNACK_BAR,
+			ErrorDetails: dtos.SnakBarErrorDetails{
+				Message: errorMsg,
+			},
+		}
+	case dtos.BOTTOM_OVERLAY:
+		errorStatusCode = 404
+		errorHandlingDetails = dtos.ErrorHandlingDetails{
+			ErrorType: dtos.BOTTOM_OVERLAY,
+			ErrorDetails: dtos.BottomOverlayErrorDetails{
+				IsFullScreen: false,
+				ImageURL:     "",
+				Heading:      "Something went wrong",
+				Content:      errorMsg,
+			},
+		}
+	}
+
+	SendJsonResponse(w, errorStatusCode, nil, errorHandlingDetails)
+}
