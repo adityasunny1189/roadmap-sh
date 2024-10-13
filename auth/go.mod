ac27c9c3cc9e1a1db166190d498915bd0ba1e71d
Aditya Pathak <adityapathak1189@gmail.com>
2024-10-13 22:19:38 +0530

    auth service

---
+++ auth/go.mod
@@ -0,0 +1,3 @@
+module github.com/adityasunny1189/roadmap-sh/auth
+
+go 1.23.2
---
+++ auth/internal/dtos/auth.go
@@ -0,0 +1,17 @@
+package dtos
+
+type SignInRequest struct {
+	
+}
+
+type SignInResponse struct {
+
+}
+
+type SignUpRequest struct {
+
+}
+
+type SignUpResponse struct {
+
+}
